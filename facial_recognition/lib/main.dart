0a85db9b5bd129858e5e4a7950514119e1a0a282
KodaIsshin <kodaisshin@gmail.com>
2025-05-04 17:23:03 -0700

    changes

--- facial_recognition/lib/main.dart
+++ facial_recognition/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:facial_recognition/pages/logo.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:facial_recognition/pages/login.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 import 'package:facial_recognition/pages/home.dart';
 import 'package:facial_recognition/pages/face_setup.dart';
@@ -11,6 +12,7 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
--- facial_recognition/lib/pages/face_setup.dart
+++ facial_recognition/lib/pages/face_setup.dart
@@ -66,7 +66,7 @@ class _FaceSetupState extends State<FaceSetup> with WidgetsBindingObserver{
     try {
     var request = http.MultipartRequest(
       'POST',
-      Uri.parse('http://127.0.0.1:5001/upload'),
+      Uri.parse('http://192.168.0.163:5001/upload'),
     );
     
     request.files.add(await http.MultipartFile.fromPath('image', imageFile.path));
